f27cc908314ddd7cdc9d49a21ef7c15647c26bbf
pedroasso <pedroasso@hotmail.com>
2022-02-12 19:45:27 -0300

    Adição da autenticação baseada em JWT

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -9,7 +9,6 @@ const usersRoute = Router();
 
 // get /users
 usersRoute.get('/users', async (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.headers['authorization']);
     const users = await userRepository.findAllUsers();
     res.status(StatusCodes.OK).send(users);
 });
